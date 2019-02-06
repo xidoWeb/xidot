c90d1a9624043898ea844b89f5296f6a7bf1a989
xido <erivery2@gmail.com>
2019-02-06 20:51:04 +0900

    index

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ko-KR">
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+  <style>
+    h1{padding: 1rem; background-color: #04f; color:#fff; text-indent: 2rem; text-transform:uppercase;}
+  </style>
+</head>
+<body>
+  <div id="wrap">
+    <h1>php test</h1>
+  </div>
+</body>
+</html>
